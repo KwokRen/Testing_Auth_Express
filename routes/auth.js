e47754c6087810a40cba7526cd7fb32acb15da1f
Kwok <kwokrenx@gmail.com>
2020-10-08 23:05:12 -0400

    used create method instead of class

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,13 @@ const router = require('express').Router();
 const User = require('../models/user')
 
 router.post('/register', async (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
+    // const user = new User({
+    //     username: req.body.username,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
     try {
+        const user = await User.create(req.body)
         const savedUser = await user.save();
         res.send(savedUser)
     } catch(err) {
