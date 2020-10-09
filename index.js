3baf72b4f1f1c57222270008012812d2fbc788c6
Kwok <kwokrenx@gmail.com>
2020-10-08 21:48:57 -0400

    added route middleware for auth

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
+// Importing the route
+const authRoute = require('./routes/auth');
 
 
 
-
-
-
-
+// Route Middleware
+app.use('/api/user', authRoute)
 
 
 app.listen(3000, () => {
